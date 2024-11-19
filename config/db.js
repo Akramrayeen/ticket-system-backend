d798fab564ddd34514c27bc08f272e3c762a3826
Akramrayeen <134880081+Akramrayeen@users.noreply.github.com>
2024-11-20 04:03:41 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,1 @@
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-})
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+
