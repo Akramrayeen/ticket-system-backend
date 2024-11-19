e7e6518e20d00674fe91e851ffa5653fae3a9c10
Akramrayeen <134880081+Akramrayeen@users.noreply.github.com>
2024-11-20 04:07:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,11 +26,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
